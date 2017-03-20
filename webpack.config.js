7cbbd36c329a7c8889ea8e4a24124490200e077a
nyo <billy31210@gmail.com>
2017-03-21 00:31:24 +0800

    create js hello world example

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
 }
