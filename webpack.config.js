aac5b39a77281d48de2f0a4ad5f719e12a5d9be6
nyo <billy31210@gmail.com>
2017-03-23 23:32:39 +0800

    update build script for production env

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
@@ -22,6 +23,9 @@ const config = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
         new HtmlWebpackPlugin({template: './src/index.html'})
     ],
     devServer: {
