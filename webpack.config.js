bb9f158612d3094ec2897a1302fee075eea5acd4
nyo <billy31210@gmail.com>
2017-04-01 01:37:23 +0800

    fix eslint error

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,51 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
-    entry: ['./src/js/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js',
-        publicPath: '/'
+  entry: ['./src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: {
+      disableDotRule: true
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.(ttf|eot|svg|woff(2)?)$/,
-                loader: 'file-loader'
-            },
-
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }),
-        new HtmlWebpackPlugin({template: './src/index.html'})
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        historyApiFallback: {
-            disableDotRule: true
-        },
-        watchOptions: {
-            ignored: /node_modules/
-        },
+    watchOptions: {
+      ignored: /node_modules/
     }
+  }
 }
 
 module.exports = config
